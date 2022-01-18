147d347750c7bf8b4f8e837c00e74fd02b8e2dc7
Bret Black <blackbret94@gmail.com>
2022-01-18 00:03:57 -0500

    Improved main menu GUI

--- Entropy/Assets/Entropy/Scripts/UI/GamePanel.cs
+++ Entropy/Assets/Entropy/Scripts/UI/GamePanel.cs
@@ -14,5 +14,20 @@ namespace Vashta.Entropy.UI
                 panel.Refresh();
             }
         }
+        
+        public virtual void SetActive(bool b)
+        {
+            gameObject.SetActive(b);
+        }
+        
+        public virtual void OpenPanel()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public virtual void ClosePanel()
+        {
+            gameObject.SetActive(false);
+        }
     }
 }

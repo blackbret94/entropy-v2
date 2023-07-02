3f372efc333b76f48fd699fc84c684ef4680c796
Bret Black <blackbret94@gmail.com>
2023-07-02 02:10:17 -0400

    Upgraded to Unity 2023

--- Wizard Cats Tank Battle/Assets/BuildReport/Scripts/Editor/ReportGeneration/BRT_UnityBuildSettingsUtility.cs
+++ Wizard Cats Tank Battle/Assets/BuildReport/Scripts/Editor/ReportGeneration/BRT_UnityBuildSettingsUtility.cs
@@ -648,7 +648,7 @@ namespace BuildReportTool
 
 			settings.AndroidBuildSubtarget = EditorUserBuildSettings.androidBuildSubtarget.ToString();
 
-			settings.AndroidUseAPKExpansionFiles = PlayerSettings.Android.useAPKExpansionFiles;
+			// settings.AndroidUseAPKExpansionFiles = PlayerSettings.Android.splitApplicationBinary;
 
 #if !UNITY_4
 			settings.AndroidAsAndroidProject = EditorUserBuildSettings.exportAsGoogleAndroidProject;
@@ -904,10 +904,10 @@ namespace BuildReportTool
 #if !UNITY_4
 			// Xbox One build settings
 			// ---------------------------------------------------------------
-			settings.XboxOneDeployMethod = EditorUserBuildSettings.xboxOneDeployMethod.ToString();
-			settings.XboxOneTitleId = PlayerSettings.XboxOne.TitleId;
-			settings.XboxOneContentId = PlayerSettings.XboxOne.ContentId;
-			settings.XboxOneProductId = PlayerSettings.XboxOne.ProductId;
+			// settings.XboxOneDeployMethod = EditorUserBuildSettings.xboxOneDeployMethod.ToString();
+			// settings.XboxOneTitleId = PlayerSettings.XboxOne.TitleId;
+			// settings.XboxOneContentId = PlayerSettings.XboxOne.ContentId;
+			// settings.XboxOneProductId = PlayerSettings.XboxOne.ProductId;
 
 #if UNITY_5_5_AND_LESSER
 			settings.XboxOneSandboxId = PlayerSettings.XboxOne.SandboxId;
@@ -925,7 +925,7 @@ namespace BuildReportTool
 
 			settings.XboxOneDisableKinectGpuReservation = PlayerSettings.XboxOne.DisableKinectGpuReservation;
 			settings.XboxOneEnableVariableGPU = PlayerSettings.XboxOne.EnableVariableGPU;
-			settings.XboxOneStreamingInstallLaunchRange = EditorUserBuildSettings.streamingInstallLaunchRange;
+			// settings.XboxOneStreamingInstallLaunchRange = EditorUserBuildSettings.streamingInstallLaunchRange;
 			settings.XboxOnePersistentLocalStorageSize = PlayerSettings.XboxOne.PersistentLocalStorageSize;
 
 			settings.XboxOneSocketNames = PlayerSettings.XboxOne.SocketNames;

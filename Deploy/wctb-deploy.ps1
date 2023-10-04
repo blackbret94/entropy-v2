082b43c331555e718aaefdf7d38c5b95e4d33f57
Bret Black <blackbret94@gmail.com>
2023-10-04 15:41:50 -0400

    Android build

---
+++ Deploy/wctb-deploy.ps1
@@ -0,0 +1,18 @@
+# Copy Windows files
+$steamDir = "G:\My Drive\Vashta Entertainment\steamworks_sdk_157_WCTB\sdk\tools\ContentBuilder\content\wctb-pt"
+$exclude = "Wizard Cats Tank Battle_BackUpThisFolder_ButDontShipItWithYourGame"
+$steamBuild = "G:\My Drive\Vashta Entertainment\steamworks_sdk_157_WCTB\sdk\tools\ContentBuilder\run_build.bat"
+
+"Copying Windows Files..."
+Copy-Item -Path "..\Wizard Cats Tank Battle\Builds\Windows\*" -Destination $steamDir -Exclude $exclude -Recurse -Force
+
+# Run Steam upload bat
+# "Uploading build..."
+# Start-Process -FilePath $steamBuild
+
+# Run AZCopy
+
+# Upload for Android
+
+# Force user to quit manually
+Read-Host -Prompt "Press Enter to exit"

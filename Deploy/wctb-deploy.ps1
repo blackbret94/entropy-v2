15aed731b1921eb46aa95b0bb964ac6da7088082
Bret Black <blackbret94@gmail.com>
2023-10-17 23:00:32 -0400

    Build 1.5.23

--- Deploy/wctb-deploy.ps1
+++ Deploy/wctb-deploy.ps1
@@ -3,7 +3,7 @@ $steamDir = "G:\My Drive\Vashta Entertainment\steamworks_sdk_157_WCTB\sdk\tools\
 $exclude = "Wizard Cats Tank Battle_BackUpThisFolder_ButDontShipItWithYourGame"
 $steamBuild = "G:\My Drive\Vashta Entertainment\steamworks_sdk_157_WCTB\sdk\tools\ContentBuilder\run_build.bat"
 "Copying Windows Files..."
-$sourcePath = "Wizard Cats Tank Battle\Builds\Windows\*"
+$sourcePath = "..\Wizard Cats Tank Battle\Builds\Windows\*"
 
 Copy-Item -Path $sourcePath -Destination $steamDir -Exclude $exclude -Recurse -Force
 # Run Steam upload bat

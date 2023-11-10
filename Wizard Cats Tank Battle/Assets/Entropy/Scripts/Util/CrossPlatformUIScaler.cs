d6062768d703052f5bf85034969c335c67ae5873
Bret Black <blackbret94@gmail.com>
2023-11-10 17:39:52 -0500

    Fixed Battle Cove, improved UX for iPad

--- Wizard Cats Tank Battle/Assets/Entropy/Scripts/Util/CrossPlatformUIScaler.cs
+++ Wizard Cats Tank Battle/Assets/Entropy/Scripts/Util/CrossPlatformUIScaler.cs
@@ -11,13 +11,30 @@ namespace Vashta.Entropy.Util
         
         private void Start()
         {
+            CanvasScaler.referenceResolution = IsLargeScreen() ? DesktopResolutionScale : MobileResolutionScale;
+        }
+
+        private bool IsLargeScreen()
+        {
+            bool isLargeScreen = false;
+
 #if UNITY_STANDALONE
-            CanvasScaler.referenceResolution = DesktopResolutionScale;
+            isLargeScreen = true;
+#endif
+            
+#if UNITY_IPHONE || UNITY_ANDROID
+            string identifier = SystemInfo.deviceModel;
+            if(identifier.StartsWith("iPhone"))
+            {
+                isLargeScreen = false;
+            }
+            else if(identifier.StartsWith("iPad"))
+            {
+                isLargeScreen = true;
+            }
 #endif
 
-#if UNITY_IPHONE || UNITY_ANDROID
-            CanvasScaler.referenceResolution = MobileResolutionScale;
-#endif
+            return isLargeScreen;
         }
     }
 }

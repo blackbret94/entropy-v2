f5e5b95705ce9200478d9f3bded4252aada33423
Bret Black <blackbret94@gmail.com>
2024-10-09 04:21:33 -0400

    More development of King of the Hill

--- Wizard Cats Tank Battle/Assets/Entropy/Scripts/GameMode/ControlPoint.cs
+++ Wizard Cats Tank Battle/Assets/Entropy/Scripts/GameMode/ControlPoint.cs
@@ -6,17 +6,18 @@ using Vashta.Entropy.ScriptableObject;
 
 namespace Vashta.Entropy.GameMode
 {
-    public class ControlPoint : MonoBehaviour
+    public class ControlPoint : MonoBehaviourPunCallbacks
     {
         // What team CONTROLS this (earns points/tick)
-        public int ControlledByTeamIndex { get; set; } = -1;
+        public int ControlledByTeamIndex { get; private set; } = -1; // SYNCED
 
         // What team is control LEANING TOWARDS (during capture)
-        public int CaptureTeamIndex { get; set; } = -1;
+        public int CaptureTeamIndex { get; private set; } = -1; // SYNCED
         public ControlPointGraphics ControlPointGraphics;
         public TeamDefinition TeamDefinitionNeutral;
 
-        private int _captureTicks = 0;
+        // How many ticks the point currently has towards capture
+        private int _captureTicks = 0; // SYNCED
         private int _ticksToCapture = 5;
         private HashSet<Player> _playersInBounds;
 
@@ -47,6 +48,12 @@ namespace Vashta.Entropy.GameMode
         {
             Init();
             
+            // ignore recalculation if no players are in bounds
+            if (_playersInBounds.Count == 0)
+            {
+                return;
+            }
+            
             // iterate over list of players.  If only ONE team is in control, set them to be the capturing team.
             // if multiple teams are present, put it into a neutral capture state
             int teamIndex = -1;
@@ -59,7 +66,10 @@ namespace Vashta.Entropy.GameMode
                         teamIndex = player.GetTeam();
                     else
                     {
-                        teamIndex = -1;
+                        // if player is on a different team stop capturing
+                        if(teamIndex != player.GetTeam())
+                            teamIndex = -1;
+                        
                         break;
                     }
                 }
@@ -71,12 +81,14 @@ namespace Vashta.Entropy.GameMode
                 // Continue capturing
                 if (teamIndex == CaptureTeamIndex)
                 {
-                    _captureTicks = Mathf.Min(_captureTicks + 1, _ticksToCapture);
+                    short numberOfTicks = (short)Mathf.Min(_captureTicks + 1, _ticksToCapture);
+                    CmdSetCaptureTicks(numberOfTicks);
                 }
                 // Uncapture towards 0
                 else
                 {
-                    _captureTicks = Mathf.Max(_captureTicks - 1, 0);
+                    short numberOfTicks = (short)Mathf.Max(_captureTicks - 1, 0);
+                    CmdSetCaptureTicks(numberOfTicks);
                 }
             }
             
@@ -86,10 +98,8 @@ namespace Vashta.Entropy.GameMode
             // Check if the state should change back to neutral
             if (_captureTicks == 0)
             {
-                CaptureTeamIndex = teamIndex;
-                
-                ControlledByTeamIndex = -1;
-                ControlPointGraphics.ChangeTeamColorControl(TeamDefinitionNeutral);
+                CmdSetCaptureTeamIndex((short)teamIndex);
+                CmdSetControlledByTeamIndex(-1);
             }
             else
             {
@@ -99,18 +109,110 @@ namespace Vashta.Entropy.GameMode
                     // Award the capture
                     if (ControlledByTeamIndex != teamIndex)
                     {
-                        ControlledByTeamIndex = teamIndex;
-                        Team team = GameManager.GetInstance().GetTeamByIndex(teamIndex);
-                        ControlPointGraphics.ChangeTeamColorControl(team.teamDefinition);
-                        ControlPointGraphics.SetFlagPosition(1);
-                        // Need to relay changes - RPC?
+                        CmdSetControlledByTeamIndex((short)teamIndex);
                     }
                 }
-                else
-                {
-                    Team team = GameManager.GetInstance().GetTeamByIndex(teamIndex);
-                    ControlPointGraphics.ChangeTeamColorCapturing(team.teamDefinition);
-                }
+            }
+        }
+
+        protected void RewardPointForHolding()
+        {
+            
+        }
+        
+        // RPC
+        // ControlledByTeamIndex
+        protected void CmdSetControlledByTeamIndex(short teamIndex)
+        {
+            // Server only
+            if (!PhotonNetwork.IsMasterClient || teamIndex == ControlledByTeamIndex)
+                return;
+            
+            this.photonView.RPC("RpcSetControlledByTeamIndex", RpcTarget.All, teamIndex);
+        }
+        
+        [PunRPC]
+        protected void RpcSetControlledByTeamIndex(short teamIndex)
+        {
+            short oldIndex = (short)ControlledByTeamIndex;
+
+            // Ignore if this is already the set team
+            if (oldIndex == teamIndex)
+                return;
+            
+            ControlledByTeamIndex = teamIndex;
+            
+            Team team = GameManager.GetInstance().GetTeamByIndex(teamIndex);
+
+            if (team != null)
+            {
+                ControlPointGraphics.ChangeTeamColorControl(team.teamDefinition);
+                ControlPointGraphics.SetFlagPosition(1);
+            }
+            else
+            {
+                Debug.LogError("Could not find team with ID: " + teamIndex);
+            }
+
+            if (teamIndex == -1)
+            {
+                // Color neutral
+                ControlPointGraphics.ChangeTeamColorControl(TeamDefinitionNeutral);
+            }
+        }
+        
+        // CaptureTicks
+        protected void CmdSetCaptureTicks(short numberOfTicks)
+        {
+            // Server only
+            if (!PhotonNetwork.IsMasterClient || numberOfTicks == _captureTicks)
+                return;
+            
+            this.photonView.RPC("RpcSetCaptureTicks", RpcTarget.All, numberOfTicks);
+        }
+
+        [PunRPC]
+        protected void RpcSetCaptureTicks(short numberOfTicks)
+        {
+            short oldTicks = (short)_captureTicks;
+
+            // Ignore if this is already the set value
+            if (oldTicks == numberOfTicks)
+                return;
+            
+            _captureTicks = numberOfTicks;
+        }
+        
+        // CaptureTeamIndex
+        protected void CmdSetCaptureTeamIndex(short teamIndex)
+        {
+            // Server only
+            if (!PhotonNetwork.IsMasterClient || teamIndex == CaptureTeamIndex)
+                return;
+            
+            this.photonView.RPC("RpcSetCaptureTeamIndex", RpcTarget.All, teamIndex);
+        }
+
+        [PunRPC]
+        protected void RpcSetCaptureTeamIndex(short teamIndex)
+        {
+            short oldTeam = (short)CaptureTeamIndex;
+
+            // Ignore if this is already the set team
+            if (oldTeam == teamIndex)
+                return;
+            
+            CaptureTeamIndex = teamIndex;
+            
+            Team team = GameManager.GetInstance().GetTeamByIndex(CaptureTeamIndex);
+
+            if (team != null)
+            {
+                ControlPointGraphics.ChangeTeamColorCapturing(team.teamDefinition);
+            }
+            else
+            {
+                Debug.LogError("Could not find team with ID: " + teamIndex);
             }
         }
 
